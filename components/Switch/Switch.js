431b3605e04a02137b51f6a5cd84f2928de9180b
claudekiseke <93127737+claudekiseke@users.noreply.github.com>
2022-07-25 16:27:05 +0100

    remove h1 switch

--- components/Switch/Switch.js
+++ components/Switch/Switch.js
@@ -2,12 +2,9 @@ import styles from "./Switch.module.scss";
 
 export default function Switch() {
     return (
-        <>
-            <h1 className={styles.heading}>Pricing</h1>
-            <label className={styles.switch}>
-                <input type="checkbox" className={styles.checkbox} />
-                <span className={styles.slider} />
-            </label>
-        </>
+        <label className={styles.switch}>
+            <input type="checkbox" className={styles.checkbox} />
+            <span className={styles.slider} />
+        </label>
     )
 }
