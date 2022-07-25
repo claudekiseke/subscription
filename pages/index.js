6741df6bfd15828f1ede419ad3b1dbedaf023aef
claudekiseke <93127737+claudekiseke@users.noreply.github.com>
2022-07-25 17:02:09 +0100

    .

--- pages/index.js
+++ pages/index.js
@@ -7,13 +7,14 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Header />
-      
+
       <main className={styles.main}>
         <h1>Display</h1>
         <div>
           <span>Flex</span>
         <Switch />
           <span>Grid</span>
+          <span className="scribble">(You know you want to)</span>
         </div>
         <Card />
         <Card />
