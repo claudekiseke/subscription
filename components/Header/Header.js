fcc1363ca9079da32104a026410754ea77456076
claudekiseke <93127737+claudekiseke@users.noreply.github.com>
2022-07-25 16:44:15 +0100

    class => className

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -3,10 +3,10 @@ import styles from "./Header.module.scss";
 export default Header = () => {
     return (
         <nav>
-            <ul class={styles.left}>
+            <ul className={styles.left}>
                 <li><a href="#">Unstyled</a></li>
             </ul>
-            <ul class={styles.right}>
+            <ul className={styles.right}>
                 <li><a href="#">Some</a></li>
                 <li><a href="#">Button</a></li>
                 <li><a href="#">Links</a></li>
