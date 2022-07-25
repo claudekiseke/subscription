6a45295b3ba5cc30b687d3c742dee86db472443f
claudekiseke <93127737+claudekiseke@users.noreply.github.com>
2022-07-25 13:49:48 +0100

    import

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Switch from '../components/Switch/Switch'
+import PricingCard from '../components/PricingCard/PricingCard'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
