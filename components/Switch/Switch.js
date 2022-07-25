70b3f366c844387c5fc198af8b666d11e1443ecb
claudekiseke <93127737+claudekiseke@users.noreply.github.com>
2022-07-25 16:06:39 +0100

    title font size

--- components/Switch/Switch.js
+++ components/Switch/Switch.js
@@ -3,7 +3,7 @@ import styles from "./Switch.module.scss";
 export default function Switch() {
     return (
         <>
-            <h1>Pricing</h1>
+            <h1 className={styles.heading}>Pricing</h1>
             <label className={styles.switch}>
                 <input type="checkbox" className={styles.checkbox} />
                 <span className={styles.slider} />
